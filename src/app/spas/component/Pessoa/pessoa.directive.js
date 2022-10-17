093d917de9cdc29f1ea93649009fd8d95d41d14c
DarleyGC2016 <garciacardoso.darley@gmail.com>
2022-10-17 16:01:57 -0300

    versao 1 - criarção do component pessoa

---
+++ src/app/spas/component/Pessoa/pessoa.directive.js
@@ -0,0 +1,10 @@
+angular.module('pessoaDirective')
+    .directive('tefefoniaPessoa', TefefoniaPessoa);
+
+    TefefoniaPessoa.$inject = [];
+
+    function TefefoniaPessoa() {
+        return {
+
+        };
+    }
